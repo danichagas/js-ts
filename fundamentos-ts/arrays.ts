297347fd7ddf0d6e853d67df1d5b8a1cefb4b39c
danichagas <chagasdaniel788@gmail.com>
2024-10-06 23:09:10 -0300

    Manipulando arrays

---
+++ fundamentos-ts/arrays.ts
@@ -0,0 +1,7 @@
+const meuArray: Array<number> = [1,2,3,4,5]
+
+const outroArrsy: string[] = ["Daniel", "Marcelly"]
+meuArray.pop()
+meuArray.push(6)
+
+const buscaNum = meuArray.find(num => num > 2)
