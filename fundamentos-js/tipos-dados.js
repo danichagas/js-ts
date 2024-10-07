1b7d6098824702c5a741e26bf24c6ff1cff08634
danichagas <chagasdaniel788@gmail.com>
2024-10-07 16:08:42 -0300

    Tipod de dados

---
+++ fundamentos-js/tipos-dados.js
@@ -0,0 +1,17 @@
+/*
+    boolean
+    string
+    null
+    number
+    symbol
+    undefined
+*/
+
+console.log(10 + '10')
+
+let nome = 'Daniel'
+let altura = 1.85
+let isBrasileiro = true
+let x
+
+console.log(x)
