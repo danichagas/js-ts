5aa3af067b094c0e4c2a13c5922fecfd9d0b211a
danichagas <chagasdaniel788@gmail.com>
2024-10-08 12:05:23 -0300

    Filter

--- js/arrays.js
+++ js/arrays.js
@@ -2,3 +2,9 @@ const lista = [1,2,3,4,5]
 lista.forEach((value, i) => {
   console.log(value, i)
 })
+
+const numerosPares = lista.filter((element) => {
+  return element % 2 === 0
+})
+
+console.log(numerosPares)
